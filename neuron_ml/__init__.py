f648a82c84822d049a89da788974e82c3a7698ec
Arthur Guiot <arguiot@gmail.com>
2018-11-03 15:59:31 +0800

    Created setup.py + init file

---
+++ neuron_ml/__init__.py
@@ -0,0 +1,11 @@
+"""
+-----------------------------------------------------
+Copyright © Arthur Guiot 2018. All rights reserved
+-----------------------------------------------------
+
+__init__.py is the core of the module. It imports all
+of the required components, so the module can be
+imported directly, without needing to do multiple
+import statements.
+
+"""
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="neuron_ml",
+    version="0.0.1",
+    author="Arthur Guiot",
+    author_email="arguiot@gmail.com",
+    description="A tiny image classification library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arguiot/Neuron",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
