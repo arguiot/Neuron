a8a88dd954d2a37d534440461a8ea8cfbcce761f
Arthur Guiot <arguiot@gmail.com>
2018-11-05 19:28:07 +0800

    Added a future test file

---
+++ test.py
@@ -0,0 +1,30 @@
+"""
+-----------------------------------------------------
+Copyright © Arthur Guiot 2018. All rights reserved
+-----------------------------------------------------
+
+test.py will train a "fake" model to check that
+everything works as it should. You can use it as a
+demo file.
+
+"""
+import neuron_ml as n
+import os
+
+# TensorFlow
+data = n.load(os.path.abspath("./dataset/"))
+model = n.train(data)
+n.export(model, os.path.abspath("./Model.pb"))
+
+# TFLite
+
+n.export(model, os.path.abspath("./Model.tflite"))
+
+# CoreML
+
+n.export(model, os.path.abspath("./Model.mlmodel"))
+
+# CreateML
+data = n.load(os.path.abspath("./dataset/"), "createml")
+model = n.train(data)
+n.export(model, os.path.abspath("./CreateML.mlmodel"))
