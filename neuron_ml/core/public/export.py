c38009b08cf0764a9e0b50948a13570e619d6b7c
Arthur Guiot <arguiot@gmail.com>
2018-11-06 19:35:04 +0800

    Added CoreML converter for TensorFlow

--- neuron_ml/core/public/export.py
+++ neuron_ml/core/public/export.py
@@ -7,25 +7,40 @@ Copyright © Arthur Guiot 2018. All rights reserved
 will be used to export the trained model.
 
 """
+
+
 def export(model, path, method="default"):
-	import shutil
-	global_path = model[0]
-	technology = model[1]
-	outputed_files = model[2]
-	if technology == "tensorflow":
-		if method == "default" and type(path) == list:
-			for i in range(outputed_files):
-				shutil.copy2(outputed_files[i], path[i])
-		else if method == "tflite":
+    import shutil
+    global_path = model[0]
+    technology = model[1]
+    outputed_files = model[2]
+    if technology == "tensorflow" and len(outputed_files) == 2:
+        if method == "default" and type(path) == list:
+            for i in range(outputed_files):
+                shutil.copy2(outputed_files[i], path[i])
+        else if method == "tflite":
 
-		else if method == "coreml":
-
-		else:
-			raise ValueError("[Neuron - Export] ERROR: Wrong arguments. See the wiki for help.")
-			return
-	else if technology == "createml" and method == "default":
-		shutil.copy2(outputed_files[0], path)
-	else:
-		raise ValueError("[Neuron - Export] ERROR: Wrong arguments. See the wiki for help.")
-		return
-	print("[Neuron - Export] All file where exported.")
+        else if method == "coreml":
+            import tfcoreml as tf_converter
+            tf_converter.convert(tf_model_path=outputed_files[0],
+                                 mlmodel_path=path,
+                                 output_feature_names=[
+                                     'softmax/logits:0'],
+                                 image_input_names=['Mul:0'],
+                                 class_labels=outputed_files[1],
+                                 red_bias=-1,
+                                 green_bias=-1,
+                                 blue_bias=-1,
+                                 image_scale=2.0 / 255.0
+                                 )
+        else:
+            raise ValueError(
+                "[Neuron - Export] ERROR: Wrong arguments. See the wiki for help.")
+            return
+    else if technology == "createml" and method == "default":
+        shutil.copy2(outputed_files[0], path)
+    else:
+        raise ValueError(
+            "[Neuron - Export] ERROR: Wrong arguments. See the wiki for help.")
+        return
+    print("[Neuron - Export] All file where exported.")
