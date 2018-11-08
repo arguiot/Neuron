67c50b17c554d0819e8d7ff1b8b5a26cff5b8903
Arthur Guiot <arguiot@gmail.com>
2018-11-08 20:10:47 +0800

    Fixed CreateML data organizer

--- neuron_ml/core/data/createml.py
+++ neuron_ml/core/data/createml.py
@@ -19,24 +19,24 @@ def organise_dataset(data, path):
 	print("[Neuron - Load] Organising dataset by moving to dataset folder & converting to jpeg")
 	for folder in folders:
 		if folder != ".git" and os.path.isdir(data + '/' + folder):
-			images = os.listdir(train + '/' + folder)
+			images = os.listdir(data + '/' + folder)
 			count = len(images)
 			n_test = int(round(count * 0.9)) # Will take 10% for tests. Max 10 images
 			if n_test > 10:
 				n_test = 10
 			r_test = range(n_test)
 
-			train = path + '/Train/'
-			test = path + '/Test/'
+			train = path + '/Train/' + folder
+			test = path + '/Test/' + folder
 			os.makedirs(train)
 			os.makedirs(test)
 			for i in range(count):
 				p = folder + '/' +images[i]
-				s = data + '/' + p
+				s = data + '/' + folder + '/' + images[i]
 				if i in r_test: # Move to test
-					d = test + p
+					d = path + '/Test/' + p
 					shutil.copy2(s, d)
 				else:
-					d = train + p
+					d = path + '/Train/' + p
 					shutil.copy2(s, d)
 	print("[Neuron - Load] Dataset folders successfully created & copied all images in corresponding folders")
