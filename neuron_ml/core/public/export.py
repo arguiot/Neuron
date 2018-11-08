6bb00080b39fb99008cb2dfa8aad96466fc2498c
Arthur Guiot <arguiot@gmail.com>
2018-11-08 19:58:34 +0800

    Trying fix bugs (again and again)

--- neuron_ml/core/public/export.py
+++ neuron_ml/core/public/export.py
@@ -45,15 +45,17 @@ def export(model, path, method="default"):
 				process = subprocess.Popen(command, stdout=subprocess.PIPE)
 				for line in iter(process.stdout.readline, b''):
 					sys.stdout.write(line)
-				output.extend([path + '/ExportedModel.mlmodel'])
-
+			else:
+				raise ValueError(
+					"[Neuron - Export] ERROR: TOCO not found. Please install TensorFlow's TOCO cli.")
+				return
 		elif method == "coreml":
 			import tfcoreml as tf_converter
 			tf_converter.convert(tf_model_path=outputed_files[0],
 								 mlmodel_path=path,
 								 output_feature_names=[
-									 'softmax/logits:0'],
-								 image_input_names=['Mul:0'],
+									 'final_result'],
+								 image_input_names=['input'],
 								 class_labels=outputed_files[1],
 								 red_bias=-1,
 								 green_bias=-1,
