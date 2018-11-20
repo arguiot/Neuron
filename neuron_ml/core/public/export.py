a9ac307bb566c41b2653c392fe2fa39be90db5f0
Arthur Guiot <arguiot@gmail.com>
2018-11-20 19:17:21 +0800

    Added TF Lite conversion

--- neuron_ml/core/public/export.py
+++ neuron_ml/core/public/export.py
@@ -20,36 +20,28 @@ def export(model, path, method="default"):
 		if method == "default" and type(path) == list:
 			for i in range(len(outputed_files)):
 				shutil.copy2(outputed_files[i], path[i])
-		# elif method == "tflite":
-		# 	import neuron_ml.tools.subprocess_checker as valid
-		# 	if valid.command("toco"):
-		# 		command = [
-		# 			"toco",
-		# 			"--graph_def_file",
-		# 			outputed_files[0],
-		# 			"--output_file",
-		# 			path,
-		# 			"--output_format",
-		# 			"TFLITE",
-		# 			"--input_shape",
-		# 			"1,299,299,3",
-		# 			"--input_arrays"
-		# 			"input",
-		# 			"--output_arrays",
-		# 			"final_result",
-		# 			"--inference_type",
-		# 			"FLOAT",
-		# 			"--input_data_type",
-		# 			"FLOAT"
-		# 		]
-		# 		print(" ".join(command))
-		# 		process = subprocess.Popen(command, stdout=subprocess.PIPE)
-		# 		for line in iter(lambda: process.stdout.read(1), b''):
-		# 			sys.stdout.buffer.write(line)
-		# 	else:
-		# 		raise ValueError(
-		# 			"[Neuron - Export] ERROR: TOCO not found. Please install TensorFlow's TOCO cli.")
-		# 		return
+		elif method == "tflite":
+			import neuron_ml.tools.subprocess_checker as valid
+			if valid.command("toco"):
+				command = [
+					"toco",
+					"--graph_def_file",
+					outputed_files[0],
+					"--output_file",
+					path,
+					"--input_arrays",
+					"Placeholder",
+					"--output_arrays",
+					"final_result"
+				]
+				print(" ".join(command))
+				process = subprocess.Popen(command, stdout=subprocess.PIPE)
+				for line in iter(lambda: process.stdout.read(1), b''):
+					sys.stdout.buffer.write(line)
+			else:
+				raise ValueError(
+					"[Neuron - Export] ERROR: TOCO not found. Please install TensorFlow's TOCO cli.")
+				return
 		elif method == "coreml":
 			import tfcoreml as tf_converter
 			tf_converter.convert(tf_model_path=outputed_files[0],
